c2b17e321516159dd3e0fb2354450e33bfbc9c04
Mat Lundin <matmll12@gmail.com>
2021-11-18 16:15:44 -0600

    fixed duration and date displays

--- public/workout.js
+++ public/workout.js
@@ -23,9 +23,11 @@ function tallyExercises(exercises) {
     if (curr.type === "resistance") {
       acc.totalWeight = (acc.totalWeight || 0) + curr.weight;
       acc.totalSets = (acc.totalSets || 0) + curr.sets;
+      acc.totalDuration = (acc.totalDuration || 0) + curr.duration;
       acc.totalReps = (acc.totalReps || 0) + curr.reps;
     } else if (curr.type === "cardio") {
       acc.totalDistance = (acc.totalDistance || 0) + curr.distance;
+      acc.totalDuration = (acc.totalDuration || 0) + curr.duration;
     }
     return acc;
   }, {});
